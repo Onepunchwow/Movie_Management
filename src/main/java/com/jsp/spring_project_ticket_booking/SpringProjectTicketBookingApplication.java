5613d655891bb50d6aba913a5e0d20490c4ef433
Pratham <nimbalkarpratham2002@gmail.com>
2025-11-25 11:57:03 +0530

    Started the Project today

---
+++ src/main/java/com/jsp/spring_project_ticket_booking/SpringProjectTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.spring_project_ticket_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/spring_project_ticket_booking/SpringProjectTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.spring_project_ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
